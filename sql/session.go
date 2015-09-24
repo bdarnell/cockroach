bc932f1b40ac30b328f4feb0d2b2a75a9d3f2d18
Vivek Menezes <vivek.menezes@gmail.com>
2015-09-24 13:57:26 -0400

    sql: SET TIME ZONE

---
+++ sql/session.go
@@ -0,0 +1,31 @@
+// Copyright 2015 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License. See the AUTHORS file
+// for names of contributors.
+//
+// Author: Vivek Menezes (vivek@cockroachlabs.com)
+
+package sql
+
+import "time"
+
+func (s Session) getLocation() (*time.Location, error) {
+	if location := s.GetLocation(); location != "" {
+		// TODO(vivek): Cache the location.
+		return time.LoadLocation(location)
+	}
+	if offset := s.GetOffset(); offset != 0 {
+		return time.FixedZone("", int(offset)), nil
+	}
+	return time.UTC, nil
+}
